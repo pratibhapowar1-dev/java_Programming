5a421bddf88f50572b0d09964da459268fe38295
Pratibha Sagar Pawar <pratibhapowar1@gmail.com>
2026-04-07 15:56:21 +0530

    simple code of additionof two numbers

---
+++ program5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i = 0, j = 0, ans = 0;
+
+    cout<<"Enter first number : \n";
+    cin>>i;
+
+    cout<<"Enter Second number : \n";
+    cin>>j;
+
+    ans = (i+j);
+
+    cout<<"Addition is : "<<ans;
+
+    return 0;
+}
