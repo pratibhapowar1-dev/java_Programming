5275c1b271b1c1d9c267f69d1f5c5b920198a0b5
Pratibha Sagar Pawar <pratibhapowar1@gmail.com>
2026-04-08 10:43:00 +0530

    Check Even Odd Numbers

---
+++ program11.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class program9
+{
+    public static boolean CheckEvenOdd(int iNo)
+    {
+      return(iNo%2 == 0);
+
+    }
+    public static void main(String A[])
+    {
+        int iValue = 0;
+        boolean bRet = true;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the number : ");
+        iValue = sobj.nextInt();
+
+        bRet = CheckEvenOdd(iValue);
+
+        if(bRet == true)
+        {
+            System.out.println(iValue +" is a even number " );
+        }
+        else
+        {
+            System.out.println(iValue + " is a odd number");
+        }
+
+    }
+}
