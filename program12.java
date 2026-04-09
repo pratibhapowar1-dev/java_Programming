644f746854224f1f2185c965b1bfa85aa042cc9d
Pratibha Sagar Pawar <pratibhapowar1@gmail.com>
2026-04-09 17:01:49 +0530

    print Jay Ganesh on screen

---
+++ program12.java
@@ -0,0 +1,21 @@
+class JayGanesh
+{
+    public static void Display()
+    {
+        System.out.println("Jay Ganesh");
+        System.out.println("Jay Ganesh");
+        System.out.println("Jay Ganesh");
+        System.out.println("Jay Ganesh");
+
+    }
+}
+
+class program12
+{
+    public static void main(String A[])
+    {
+        JayGanesh jobj = new JayGanesh();
+
+        jobj.Display();
+    }
+}
