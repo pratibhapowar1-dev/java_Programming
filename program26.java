78d5fc3403f8cadbbaa1cd2c157615863cab0e21
Pratibha Sagar Pawar <pratibhapowar1@gmail.com>
2026-04-11 20:08:10 +0530

    Check Divisible or nut number

---
+++ program26.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Divisible
+{
+    public boolean CheckDivisible(int iNo1, int iNo2)
+    {
+        if(iNo1 % iNo2 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+class program26
+{
+    public static void main(String A[])
+    {
+        int iValue1 = 0, iValue2 = 0;
+        boolean bRet = false;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the first number : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter thr second number :");
+        iValue2 = sobj.nextInt();
+
+        Divisible dobj = new Divisible();
+
+        bRet = dobj.CheckDivisible(iValue1, iValue2);
+
+        if(bRet == true)
+        {
+            System.out.println("is a completly Divisible number ");
+        }
+        else
+        {
+            System.out.println("is not a divisible number ");
+        }
+
+    }
+}
