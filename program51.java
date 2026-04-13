4e33f2b103da84fa649c54307acce5f6acc6b2c6
Pratibha Sagar Pawar <pratibhapowar1@gmail.com>
2026-04-13 16:10:25 +0530

    count factors

---
+++ program51.java
@@ -0,0 +1,67 @@
+
+import java.util.*;
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : CountFactors
+// Description   : it is used count Non factors
+// Input         : Intiger
+// Output        : Intiger
+// Author        : Pratibha Sagar Pawar 
+//    
+////////////////////////////////////////////////////////////////////////////////////////////
+class Display
+{
+    public void CountFactors(int iNo)
+    {
+        int iCnt = 0, iFrequancy = 0;
+
+        if(iNo<0)
+        {
+            iNo = -iNo;
+        }
+
+        for(iCnt = 1; iCnt<=(iNo/2); iCnt++)
+        {
+            if(iNo%iCnt==0)
+            {
+                iFrequancy++;
+            }
+           
+        }
+
+        System.out.println("Count of facters : "+iFrequancy);
+    }
+}
+///////////////////////////////////////////////////////////////////
+//  Entry point function
+//////////////////////////////////////////////////////////////////
+class program51
+{
+    public static void main(String A[])
+    {
+       int iValue = 0, iCount = 0;
+
+       Scanner sobj  =new Scanner(System.in);
+
+       System.out.println("Enter the number : ");
+
+       iValue = sobj.nextInt();
+
+       Display dobj = new Display();
+
+       dobj.CountFactors(iValue);
+
+
+
+
+    }
+
+
+///////////////////////////////////////////////////////////////////////////////
+//  End of entry point function
+///////////////////////////////////////////////////////////////////////////////
+}
+///////////////////////////////////////////////////////////////////////////////
+//  End of class
+///////////////////////////////////////////////////////////////////////////////
