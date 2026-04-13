8a6ae4647defddf05cc163e790b6371726703b47
Pratibha Sagar Pawar <pratibhapowar1@gmail.com>
2026-04-13 16:05:14 +0530

    Display factors

---
+++ program48.java
@@ -0,0 +1,63 @@
+
+import java.util.*;
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : DisplayFactors
+// Description   : it is used display factors
+// Input         : Intiger
+// Output        : Intiger
+// Author        : Pratibha Sagar Pawar 
+//    
+////////////////////////////////////////////////////////////////////////////////////////////
+class Display
+{
+    public void DisplayFactors(int iNo)
+    {
+        int iCnt = 0;
+
+        if(iNo<0)
+        {
+            System.out.println("Invalid Input");
+        }
+
+// Time complexity : o(N/n)
+
+        for(iCnt = 1; iCnt<(iNo/2); iCnt++)
+        {
+            if(iNo % iCnt == 0)
+            {
+                System.out.print(iCnt+"\t");
+            }
+          
+        }
+
+    }
+
+///////////////////////////////////////////////////////////////////
+//  Entry point function
+//////////////////////////////////////////////////////////////////
+
+    public static void main(String A[])
+    {
+       int iValue = 0;
+
+       Scanner sobj  =new Scanner(System.in);
+
+       System.out.println("Enter the number : ");
+
+       iValue = sobj.nextInt();
+
+       Display dobj = new Display();
+
+       dobj.DisplayFactors(iValue);
+
+
+    }
+///////////////////////////////////////////////////////////////////////////////
+//  End of entry point function
+///////////////////////////////////////////////////////////////////////////////
+}
+///////////////////////////////////////////////////////////////////////////////
+//  End of class
+///////////////////////////////////////////////////////////////////////////////
